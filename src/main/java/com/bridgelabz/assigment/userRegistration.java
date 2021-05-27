c3b8bf6b13c32794775e3b3d416ffc0a8766b694
dhr231 <dhruvprashar231@gmail.com>
2021-05-27 22:25:20 +0530

    Input and validate User's First Name

--- src/main/java/com/bridgelabz/assigment/userRegistration.java
+++ src/main/java/com/bridgelabz/assigment/userRegistration.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.assignment;
 import java.util.*;
 public class userRegistration {
-    public static void main(String[] args) {
+    boolean validateFirstName(String firstName){
+        Pattern regex=Pattern.compile(^[A-Z][a-z]{3,});
+        return regex.matcher(firstName).matches;
 
+    }
+
+//    boolean validateLastName(String lastName){
+//        Pattern regex=Pattern.
+//    }
+    public static void main(String[] args) {
+    Pattern firstName=Pattern.compile([A-Z])
     }
 }
